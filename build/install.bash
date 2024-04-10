6629ac1fc19ab505cd239b7f72fd5c706042c3a6
Vladimir Zorin <vladimir@deviant.guru>
2024-04-10 18:32:26 +0300

    Add build scripts

---
+++ build/install.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -euo pipefail
+
+wolfssl_tag="v5.6.6-stable"
+
+cd /src
+
+# Build WolfSSL
+git clone --depth 1 -b "${wolfssl_tag}" https://github.com/wolfSSL/wolfssl.git
+cd wolfssl
+./autogen.sh
+# use `--enable-debug` for dev builds
+./configure --build=x86_64 --host=x86_64 --enable-curve25519 --enable-ed25519 --disable-oldtls --enable-tls13 --enable-static --enable-sni --enable-altcertchains
+make && make install
+
+# Build LuaJIT
+cd /src
+git clone https://github.com/LuaJIT/LuaJIT
+cd LuaJIT
+git checkout v2.1
+make XCFLAGS="-DLUAJIT_DISABLE_FFI -DLUAJIT_ENABLE_LUA52COMPAT"
+make install
+
+# Build Lilush
+cd /src/lilush/build
+./make.sh all
---
+++ build/make.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+set -euo pipefail
+
+base_dir=${PWD%/build}
+dirs=(luasocket std crypto term text djot redis llm shell dns argparser markdown)
+
+headers_from_luamod () {
+    file_name="${1##*/}"
+    mod_name="${file_name%%.lua}"
+    luajit -b "${1}" "mod_lua_${mod_name}.h"
+    sed -i 's/#define/const size_t/;1s/$/;/;s/luaJIT_BC/mod_lua/g;s/unsigned //;s/SIZE /SIZE=/' "mod_lua_${mod_name}.h"
+}
+
+do_clean () {
+    cd ${base_dir}/src/lua-cjson && make clean && cd -
+    cd ${base_dir}/src/luasocket && make clean && cd -
+    cd ${base_dir}/src/std && make clean && cd -
+    cd ${base_dir}/src/crypto && make clean && cd -
+    cd ${base_dir}/src/wireguard && make clean && cd -
+    cd ${base_dir}/src/term && make clean && cd -
+    cd ${base_dir}/build
+    for d in ${dirs[@]}; do
+        rm -rf ${d}
+    done
+    rm lilush liblilush.a
+}
+
+do_build () {
+    cd ${base_dir}/src/lua-cjson && make && strip --strip-debug --strip-unneeded *.o && cd -
+    cd ${base_dir}/src/std && make && strip --strip-debug --strip-unneeded *.o && cd -
+    cd ${base_dir}/src/crypto && make && strip --strip-debug --strip-unneeded *.o && cd -
+    cd ${base_dir}/src/term && make && strip --strip-debug --strip-unneeded *.o && cd -
+    cd ${base_dir}/src/wireguard && make && strip --strip-debug --strip-unneeded *.o && cd -
+    cd ${base_dir}/src/luasocket && make linux && strip --strip-debug --strip-unneeded *.o && cd -
+}
+
+do_headers() {
+    cd ${base_dir}/build
+    for d in ${dirs[@]}; do
+        [[ ! -d ${d} ]] && mkdir ${d}
+        cd ${d}
+        for f in ${base_dir}/src/${d}/*.lua; do
+            headers_from_luamod ${f}
+        done
+        cd ..
+    done
+}
+
+do_linking() {
+    ar rcs liblilush.a ${base_dir}/src/lua-cjson/*.o ${base_dir}/src/luasocket/*.o ${base_dir}/src/std/*.o ${base_dir}/src/crypto/*.o ${base_dir}/src/term/*.o ${base_dir}/src/wireguard/*.o
+    clang -Os -s -O3 -Wall -Wl,-E -o lilush ${base_dir}/src/lilush.c -I/usr/local/include/luajit-2.1 -I/usr/local/include/wolfssl -L/usr/local/lib -lluajit-5.1 -Wl,--whole-archive -lwolfssl liblilush.a -Wl,--no-whole-archive -static
+}
+
+do_install () {
+    cp lilush /bin/
+    if ! grep lilush /etc/shells &> /dev/null; then
+        echo "/bin/lilush" >> /etc/shells
+    fi
+}
+
+builtins=(netstat dig digg files_matching envlist kat)
+
+create_symlinks () {
+    cd /bin/
+    for builtin in ${builtins[@]}; do
+        [[ ! -a ${builtin} ]] && ln -s lilush ${builtin}
+    done
+}
+
+case ${1} in
+    clean)
+        do_clean
+        ;;
+    install)
+        do_install
+        ;;
+    build)
+        do_build
+        ;;
+    headers)
+        do_headers
+        ;;
+    link)
+        do_linking
+        ;;    
+    it)
+        do_build
+        do_headers
+        do_linking
+        ;;
+    symlinks)
+        create_symlinks
+        ;;
+    all)
+        do_build
+        do_headers
+        do_linking
+        do_install
+        create_symlinks
+        ;;
+    *)
+        echo "Uknown option. Valid variants: clean, build, headers, link, install, all"
+        exit 1
+        ;;
+esac
